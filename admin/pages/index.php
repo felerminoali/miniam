27d6cb5c8998d929766423dea1e351b32405ee10
felerminoali <felerminoali@gmail.com>
2018-12-29 08:39:31 +0200

    29-12-2018

--- admin/pages/index.php
+++ admin/pages/index.php
@@ -24,9 +24,9 @@ require_once('template/_header.php')
 ?>
 
 <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-    <button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<!--    <button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--        <span class="navbar-toggler-icon"></span>-->
+<!--    </button>-->
     <a class="navbar-brand" href="#">Dashboard</a>
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault" style="float: right;">
