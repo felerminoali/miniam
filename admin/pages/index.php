1471b93db34ec89e168c95d97b1019e9a4fb1a73
felerminoali <felerminoali@gmail.com>
2018-12-29 08:37:48 +0200

    29-12-2018

--- admin/pages/index.php
+++ admin/pages/index.php
@@ -41,31 +41,14 @@ require_once('template/_header.php')
 <div class="container-fluid">
     <div class="row">
         <nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
-<!--            <ul class="nav nav-pills flex-column">-->
-<!--                <li class="nav-item">-->
-<!--                    <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>-->
-<!--                </li>-->
-<!--                <li class="nav-item">-->
-<!--                    <a class="navbar-link" href="#">Reports</a>-->
-<!--                </li>-->
-<!--                <li class="nav-item">-->
-<!--                    <a class="nav-link" href="#">Analytics</a>-->
-<!--                </li>-->
-<!--                <li class="nav-item">-->
-<!--                    <a class="nav-link" href="#">Export</a>-->
-<!--                </li>-->
-<!--            </ul>-->
 
 
             <ul class="nav nav-pills flex-column">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Nav item again</a>
+                    <a class="nav-link" href="#">Manage Cases</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">One more nav</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Another nav item</a>
+                    <a class="nav-link" href="#">Manage Laws</a>
                 </li>
             </ul>
         </nav>
