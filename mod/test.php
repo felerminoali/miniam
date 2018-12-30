d9ef5efb50fae555c8a00cea1c9824f0a080dafd
felerminoali <felerminoali@gmail.com>
2018-12-30 17:36:10 +0200

    30-12-2018

--- mod/test.php
+++ mod/test.php
@@ -6,18 +6,20 @@ $objCases = new Cases();
 
 $cases= $objCases->getAllCases();
 
+$casesArray = array();
 foreach ($cases as $row){
     $dataArray = array();
     $dataArray[] = $row["id"];
     $dataArray[] = $row["name"];
     $dataArray[] = $row["url"];
-    $dataArray[] = $row["summary"];
+    $dataArray[]= $row["summary"];
+    $casesArray[] = $dataArray;
 }
 
 //echo json_encode($cases);
 
 $fp = fopen(ROOT_PATH . DS . "log" . DS . "error.log", 'a');
-fwrite($fp, 'Size: '.count($dataArray));
+fwrite($fp, 'Size: '.count($casesArray));
 fclose($fp);
 
 
