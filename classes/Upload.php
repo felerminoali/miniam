188faeac3e8fdb04e82c2e3e173975c0ca8c9fec
felerminoali <felerminoali@gmail.com>
2018-12-31 12:41:55 +0200

    30-12-2018

--- classes/Upload.php
+++ classes/Upload.php
@@ -28,7 +28,7 @@ class Upload
 
 
         $fp = fopen(ROOT_PATH . DS . "log" . DS . "error.log", 'a');
-        fwrite($fp, is_dir("../../../media"));
+        fwrite($fp, is_dir("../../".DS));
 //        fwrite($fp, realpath(dirname("../../")));
         fclose($fp);
 
