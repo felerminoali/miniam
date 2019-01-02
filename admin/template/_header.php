6480a00d975445073e567dced1dce719a5edc2d2
felerminoali <felerminoali@gmail.com>
2019-01-02 11:22:51 +0200

    01-02-2018

--- admin/template/_header.php
+++ admin/template/_header.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-<!--    <link rel="icon" href="../../favicon.ico">-->
+    <!--    <link rel="icon" href="../../favicon.ico">-->
 
     <title>Dashboard Template for Bootstrap</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/dashboard/">
@@ -16,15 +15,15 @@
     <link href="../css/datatables.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-<!--    <link href="dashboard.css" rel="stylesheet">-->
-<!--    <script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/3D2BD7C9-A21D-5B4B-BADB-315F5013C30C/main.js" charset="UTF-8"></script></head>-->
+    <!--    <link href="dashboard.css" rel="stylesheet">-->
+    <!--    <script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/3D2BD7C9-A21D-5B4B-BADB-315F5013C30C/main.js" charset="UTF-8"></script></head>-->
 
 <body>
 
 <nav class="navbar navbar-default navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-             <a class="navbar-brand" href="/admin/">Dashboard</a>
+            <a class="navbar-brand" href="/admin/">Dashboard</a>
         </div>
 
         <div id="navbar" class="navbar-collapse collapse">
@@ -32,9 +31,11 @@
                 <li><a href="/">Home</a></li>
             </ul>
         </div>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="/admin/?page=logout">Logout</a></li>
-        </ul>
+        <div class="container-fluid" style="margin-top: 0.60em;">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="/admin/?page=logout">Logout</a></li>
+            </ul>
+        </div>
     </div>
 </nav>
 
@@ -45,12 +46,12 @@
 <div class="container">
     <div class="row">
 
-            <div class="col-sm-3 col-md-2 sidebar">
-                <ul class="nav nav-sidebar">
-                    <li class="active"><a href="/admin/">Dashboard <span class="sr-only">(current)</span></a></li>
-                    <li><a href="/admin/?page=listcase">Manage Cases</a></li>
-                    <li><a  href="/admin/?page=listlaw">Manage Laws</a></li>
-                </ul>
+        <div class="col-sm-3 col-md-2 sidebar">
+            <ul class="nav nav-sidebar">
+                <li class="active"><a href="/admin/">Dashboard <span class="sr-only">(current)</span></a></li>
+                <li><a href="/admin/?page=listcase">Manage Cases</a></li>
+                <li><a href="/admin/?page=listlaw">Manage Laws</a></li>
+            </ul>
 
 
-            </div>
+        </div>
