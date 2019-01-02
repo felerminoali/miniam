a6b3ae0b045bdeba79e1f56895d9ed5e8e8ce95f
felerminoali <felerminoali@gmail.com>
2019-01-02 09:52:15 +0200

    01-02-2018

--- admin/template/_header.php
+++ admin/template/_header.php
@@ -38,7 +38,7 @@
         <nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
 
 
-            <ul class="nav nav-pills flex-column">
+            <ul class="nav nav-pills nav-fill flex-column">
                 <li class="nav-item">
                     <a class="nav-link active" href="/admin/?page=listcase">Manage Cases</a>
                 </li>
